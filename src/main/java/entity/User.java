ef4556d8ca60c93b805639c30d2ddbe6ff20c23f
KatlaTL <balloupjuske2208@gmail.com>
2017-05-11 11:27:13 +0200

    added reservation to user entity

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -7,46 +7,64 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import security.IUser;
 import security.PasswordStorage;
 
 @Entity(name = "SEED_USER")
-public class User implements IUser, Serializable{
- 
-  //You will need to change this to save a Hashed/salted password 
-  @Column(length = 255, name = "PASSWORD_HASH",nullable = false)
-  private String passwordHash; 
-  
-  @Id
-  @Column(length = 35, name = "USER_NAME",nullable = false)
-  private String userName;
-  
-  @ManyToMany
-  List<Role> roles;
- 
-  public User() {
-  }
- 
-  public User(String userName, String password) throws PasswordStorage.CannotPerformOperationException {
-    this.userName = userName;
-    this.passwordHash = PasswordStorage.createHash(password);
-  }
-  
-  public void addRole(Role role){
-    if(roles == null){
-      roles = new ArrayList();
+public class User implements IUser, Serializable {
+
+    //You will need to change this to save a Hashed/salted password 
+    @Column(length = 255, name = "PASSWORD_HASH", nullable = false)
+    private String passwordHash;
+
+    @Id
+    @Column(length = 35, name = "USER_NAME", nullable = false)
+    private String userName;
+
+    @ManyToMany
+    List<Role> roles;
+
+    @OneToMany
+    List<FlightReservation> reservations;
+
+    public User() {
     }
-    roles.add(role);
-    role.addUser(this);
-  }
-  
-  public List<Role> getRoles(){
-    return roles;
-  }
-    
-  @Override
-  public List<String> getRolesAsStrings() {
-   if (roles.isEmpty()) {
+
+    public User(String userName, String password) throws PasswordStorage.CannotPerformOperationException {
+        this.userName = userName;
+        this.passwordHash = PasswordStorage.createHash(password);
+    }
+
+    public User(String passwordHash, String userName, List<FlightReservation> reservations) {
+        this.passwordHash = passwordHash;
+        this.userName = userName;
+        this.reservations = reservations;
+    }
+
+    public List<FlightReservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<FlightReservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    public void addRole(Role role) {
+        if (roles == null) {
+            roles = new ArrayList();
+        }
+        roles.add(role);
+        role.addUser(this);
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public List<String> getRolesAsStrings() {
+        if (roles.isEmpty()) {
             return null;
         }
         List<String> rolesAsStrings = new ArrayList();
@@ -54,20 +72,20 @@ public class User implements IUser, Serializable{
             rolesAsStrings.add(role.getRoleName());
         }
         return rolesAsStrings;
-  }
- 
-  @Override
-  public String getPassword() {
-    return passwordHash;
-  }
-  
-  public void setPassword(String password) throws PasswordStorage.CannotPerformOperationException {
-    this.passwordHash = this.passwordHash = PasswordStorage.createHash(password);
-  }
+    }
 
-  @Override
-  public String getUserName() {
-    return userName;
-  }
-     
+    @Override
+    public String getPassword() {
+        return passwordHash;
+    }
+
+    public void setPassword(String password) throws PasswordStorage.CannotPerformOperationException {
+        this.passwordHash = this.passwordHash = PasswordStorage.createHash(password);
+    }
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
 }
